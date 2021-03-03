928d36e13766ef15dace40b676b52b3aca9fc14c
Steven McStravog <steven.mcstravog@gmail.com>
2021-03-03 20:28:00 +0000

    Minor tidy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { QUESTIONS as questions } from './constants';
 
