9a3f3c77792c0b7c241e53669e13e60fa7db2500
Steven McStravog <steven.mcstravog@gmail.com>
2021-03-03 20:15:07 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { QUESTIONS as questions } from './constants';
 
-function App() {
+const App = () => {
+
+  const [quizQuestions, setQuizQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isQuizFinished, setFinishQuiz] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    setQuestions();
+  }, [])
+
+  const setQuestions = () => {
+    shuffleQuestions();
+    setQuizQuestions(questions.slice(0, 5));
+  }
+
+  const shuffleQuestions = () => {
+    for (let i = questions.length - 1; i > 0; i--) {
+      const s = Math.floor(Math.random() * (i + 1));
+      [questions[i], questions[s]] = [questions[s], questions[i]];
+    }
+  }
+
+  const handleAnswerClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    (nextQuestion < quizQuestions.length) ? setCurrentQuestion(nextQuestion) : setFinishQuiz(true);
+  }
+
+  const handleQuizRestart = () => {
+    setQuestions();
+    setCurrentQuestion(0);
+    setScore(0);
+    setFinishQuiz(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {quizQuestions.length > 0 &&
+        <Container className="quiz">
+          <Row>
+            <Col sm={12} md={8} className="mx-auto">
+              <h1 className="title"><span>Horror</span> Movie Quiz</h1>
+              {isQuizFinished
+                ? <>
+                  <h5 className="mt-4">You answered</h5>
+                  <h1 className="question-count">{score} / {quizQuestions.length}</h1>
+                  <h5>questions correct</h5>
+                  <Button variant="success" className="mt-4" size="lg" onClick={handleQuizRestart}>Play Again</Button>
+                </>
+                : <>
+                  <div className="question-section mt-4 mb-5">
+                    <h5 className="question-count">{`${currentQuestion + 1} / ${quizQuestions.length}`}</h5>
+                    <h4>{quizQuestions[currentQuestion].question}</h4>
+                  </div>
+                  <div className="answer-section">
+                    {quizQuestions[currentQuestion].answers.map((answer) => (
+                      <button onClick={() => handleAnswerClick(answer.isCorrect)}>{answer.text}</button>
+                    ))}
+                  </div>
+                </>
+              }
+            </Col>
+          </Row>
+        </Container>
+      }
+    </>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,91 @@
+export const QUESTIONS = [
+    {
+        question: 'Which actor made his film debut in A Nightmare on Elm Street?',
+        answers: [
+            { text: 'David Arquette', isCorrect: false },
+            { text: 'Ryan Reynolds', isCorrect: false },
+            { text: 'Johnny Depp', isCorrect: true },
+            { text: 'Jared Leto', isCorrect: false }
+        ]
+    },
+    {
+        question: 'Which actor played the killer in Se7en?',
+        answers: [
+            { text: 'Brad Pitt', isCorrect: false },
+            { text: 'Morgan Freeman', isCorrect: false },
+            { text: 'John Cusack', isCorrect: false },
+            { text: 'Kevin Spacey', isCorrect: false }
+        ]
+    },
+    {
+        question: 'Who was the murderer in the 1980 slasher Friday the 13th?',
+        answers: [
+            { text: 'Pamela Voorhees', isCorrect: true },
+            { text: 'Michael Myers', isCorrect: false },
+            { text: 'Jason Voorhees', isCorrect: false },
+            { text: 'Freddy Krueger', isCorrect: false }
+        ]
+    },
+    {
+        question: 'Michael Myer\'s mask in 1978 movie Halloween is based on which actor\'s face?',
+        answers: [
+            { text: 'James Dean', isCorrect: false },
+            { text: 'Burt Reynolds', isCorrect: false },
+            { text: 'William Shatner', isCorrect: true },
+            { text: 'Richard Dreyfuss', isCorrect: false }
+        ]
+    },
+    {
+        question: 'Which horror movie had the tagline \"Man is the warmest place to hide\"?',
+        answers: [
+            { text: 'Alien', isCorrect: false },
+            { text: 'The Thing', isCorrect: true },
+            { text: 'Re-Animator', isCorrect: false },
+            { text: 'The Fly', isCorrect: false }
+        ]
+    },
+    {
+        question: 'Which horror movie did the monster/villain attack high school kids through their dreams?',
+        answers: [
+            { text: 'The Texas Chainsaw Massacre', isCorrect: false },
+            { text: 'The Exorcist', isCorrect: false },
+            { text: 'A Nightmare on Elm Street', isCorrect: true },
+            { text: 'The Shining', isCorrect: false }
+        ]
+    },
+    {
+        question: 'What classic horror movie was originally titled The Babysitter Murders?',
+        answers: [
+            { text: 'Halloween', isCorrect: true },
+            { text: 'Scream', isCorrect: false },
+            { text: 'A Nightmare on Elm Street', isCorrect: false },
+            { text: 'Friday the 13th', isCorrect: false }
+        ]
+    },
+    {
+        question: 'How many killers are revealed at the end of the movie Scream?',
+        answers: [
+            { text: '1', isCorrect: false },
+            { text: '2', isCorrect: true },
+            { text: '3', isCorrect: false },
+            { text: '4', isCorrect: false }
+        ]
+    },
+    {
+        question: 'In the movie Scream what is Rule #1 on Randy\'s list of rules for surviving a horror movie?',
+        answers: [
+            { text: 'Say I\'ll be right back', isCorrect: false },
+            { text: 'Don\'t drink or do drugs', isCorrect: false },
+            { text: 'Don\'t have sex', isCorrect: true }
+        ]
+    },
+    {
+        question: 'Which of the following movies was not based on a Stephen King book?',
+        answers: [
+            { text: 'Misery', isCorrect: false },
+            { text: 'The Exorcist', isCorrect: true },
+            { text: 'Cujo', isCorrect: false },
+            { text: 'Children of the Corn', isCorrect: false }
+        ]
+    }
+]
